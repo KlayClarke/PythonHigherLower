e7b9df348c2e277e1b77039ca3eeb392a5717aa3
Klay Clarke <klayaclarke@gmail.com>
2021-11-11 14:11:48 -0500

    shift prompt so last person of the first prompt becomes the first person of the next

--- higher_lower.py
+++ higher_lower.py
@@ -7,33 +7,34 @@ def play():
     #set variable of score equal to 0 as an official restart of score
     score = 0
     tries = 0
+    random_account_b = random.choice(list(data))
     while score < 10:
         tries += 1
         #ask user to compare two entries from data
-        random_one = random.choice(list(data))
-        random_two = random.choice(list(data))
+        random_account_a = random_account_b
+        random_account_b = random.choice(list(data))
         # 'Compare {random entry 1}'
-        print(f'Compare A: {random_one["name"]}, a {random_one["description"]} from {random_one["country"]}\n')
+        print(f'Compare A: {random_account_a["name"]}, a {random_account_a["description"]} from {random_account_a["country"]}\n')
         ####    #show vs sign
         print(vs)
         ##### 'to {random entry 2}'
-        print(f'Against B: {random_two["name"]}, a {random_two["description"]} from {random_two["country"]}\n')
+        print(f'Against B: {random_account_b["name"]}, a {random_account_b["description"]} from {random_account_b["country"]}\n')
         #remove the previous dicts from list to prevent duplicates
         for person in data:
-            if person['name'] == random_one['name']:
+            if person['name'] == random_account_a['name']:
                 data.remove(person)
-            elif person['name'] == random_two['name']:
+            elif person['name'] == random_account_b['name']:
                 data.remove(person)
         ####Ask user who has more follower
         answer = input(f'Who do you think has more followers? Type either A or B: \n').lower()
         if answer == 'a':
-            if random_one['follower_count'] > random_two['follower_count']:
+            if random_account_a['follower_count'] > random_account_b['follower_count']:
                 score += 1
                 print(f'You\'re right!\nCurrent score: {score}\n')
             else:
                 print(f'Sorry, but that\'s wrong!\nCurrent score: {score}\n')
         elif answer == 'b':
-            if random_two['follower_count'] > random_one['follower_count']:
+            if random_account_b['follower_count'] > random_account_a['follower_count']:
                 score += 1
                 print(f'You\'re right!\nCurrent score: {score}\n')
             else:
@@ -41,6 +42,7 @@ def play():
             
     if score == 10:
         print(f'It took you {tries} tries to get {score} answers right. Good job!\n')
+
     replay = int(input('Type 1 to replay. Type 2 otherwise.\n'))
     if replay == 1:
         play()
